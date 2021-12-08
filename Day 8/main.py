172564824e38dd1ac5ea7bf30b0b4c003ee13a88
TiBiBa <timonbakker@hotmail.com>
2021-12-08 14:59:54 +0100

    Finished day 8

--- Day 8/main.py
+++ Day 8/main.py
@@ -11,15 +11,17 @@ def step1():
 
 def get_empty_decoder():
     decoder = {}
-    for i in range(1, 8):
+    for i in range(0, 10):
         decoder[i] = ""
     return decoder
 
 def step2():
     file = open("input.txt", "r")
     puzzles = file.read().splitlines()
+
+    counter = 0
+    puzzle_count = 0
     for puzzle in puzzles:
-        decoder = get_empty_decoder()
         numbers = get_empty_decoder()
 
         puzzle_input = puzzle.split("|")[0].split()
@@ -28,6 +30,9 @@ def step2():
         puzzle_output = puzzle.split("|")[1].split()
         puzzle_output = [''.join(sorted(x)) for x in puzzle_output]
 
+        letters = "abcdefg"
+
+        #Get the numbers we know for sure
         for number in puzzle_input:
             if len(number) == 2:
                 numbers[1] = number
@@ -37,10 +42,47 @@ def step2():
                 numbers[4] = number
             elif len(number) == 7:
                 numbers[8] = number
-        decoder[1] = numbers[7]
-        for char in numbers[1]:
-            if char in decoder[1]:
-                decoder[1] = decoder[1].replace(char, '')
-        print(decoder)
+
+        #Find the 6 by looking if a len(6) has a bit that the 7 does NOT have
+        for number in puzzle_input:
+            if len(number) == 6:
+                for char in numbers[7]:
+                    if char not in number:
+                        numbers[6] = number
+
+        #Find the 2, 3 and 5 (They are all 5 char long)
+        for number in puzzle_input:
+            if len(number) == 5:
+                temp = numbers[6]
+                for char in number:
+                    temp = temp.replace(char, "")
+                if len(temp) == 1:
+                    numbers[5] = number
+                elif len(temp) == 2:
+                    for sub_char in temp:
+                        if sub_char in numbers[1]:
+                            numbers[2] = number
+                    if (numbers[2] != number):
+                        numbers[3] = number
+
+        #Find the 0 and 9
+        for number in puzzle_input:
+            if len(number) == 6:
+                temp = number
+                for char in numbers[4]:
+                    temp = temp.replace(char, "")
+                if len(temp) == 2:
+                    numbers[9] = number
+                elif (numbers[6] != number):
+                    numbers[0] = number
+
+        number_string = ""
+        for decoded_number in puzzle_output:
+            for key, char_string in numbers.items():
+                if decoded_number == char_string:
+                    number_string += str(key)
+
+        counter += int(number_string)
+    print(counter)
 
 step2()
