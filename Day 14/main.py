7ab6c0ca8da263bae268dce2a6b81282fcb18089
Timon Bakker <timonbakker@hotmail.com>
2021-12-14 12:28:02 +0100

    Implemented day 14 part 1

---
+++ Day 14/main.py
@@ -0,0 +1,40 @@
+
+def replace_mutations_by_dict(mutations):
+    mutations_dict = {}
+    for mutation in mutations:
+        start = mutation.split("->")[0][:-1]
+        end = mutation.split("->")[1][1:]
+
+        mutations_dict[start] = end
+    return mutations_dict
+
+
+def check_mutation(mutations, substring):
+    if substring in mutations:
+        return mutations[substring]
+    else:
+        return None
+
+
+def step1():
+    file = open("input.txt", "r")
+    file_data = file.read().splitlines()
+    template = file_data[0]
+    mutations = replace_mutations_by_dict(file_data[2:])
+
+    for _ in range(0, 10):
+        substrings = []
+        for i in range(0, len(template)-1):
+            substrings.append(template[i:i+2])
+        for substring in substrings:
+            mutation = check_mutation(mutations, substring)
+            if mutation:
+                location = template.find(substring)
+                template = template[:location+1] + mutation + template[location+1:]
+    for char in set(template):
+        print(char + " has " + str(template.count(char)) + " occurrences")
+
+def step2():
+    pass
+
+step1()
